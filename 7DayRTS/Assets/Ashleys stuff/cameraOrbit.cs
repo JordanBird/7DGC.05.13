1e7d4f04bfbc3a366d531e4bdc42f604bb77f747
Ashley Blake-Hood <ashbh@hotmail.co.uk>
2013-05-25 17:36:24 +0100

    Updated Camera Script

--- 7DayRTS/Assets/Ashleys stuff/cameraOrbit.cs
+++ 7DayRTS/Assets/Ashleys stuff/cameraOrbit.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class cameraOrbit : MonoBehaviour {
 	
-	Vector3 mousePos;
+	float mouseXPos;
+	float rotateSpeed;
+	public float rotSpeed;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,22 +13,36 @@ public class cameraOrbit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		mousePos = Input.mousePosition;
+		mouseXPos = Input.mousePosition.x;
+		rotateSpeed = setRotSpeed();
+		Debug.Log(rotateSpeed);
+		Debug.Log(-rotateSpeed);
+		//rotateSpeed = mouseXPos * rotSpeed * 0.02f; 
 	if (Input.GetKey(KeyCode.Mouse2))
 		{
 			orbit();
 		}				
 	}
 	
+	float setRotSpeed()
+	{
+		float rotSpeed;
+		float screenCenter = Screen.width/2;
+		
+		rotSpeed = (screenCenter - mouseXPos) * 0.01f;
+		
+		return rotSpeed;
+	}
+	
 	void orbit()
 	{
-		if (mousePos.x > (Screen.width/2 + 50))
+		if (mouseXPos > (Screen.width/2 + 50))
 		{
-			this.transform.Rotate(0,2,0);
+			this.transform.Rotate(0,-rotateSpeed,0);
 		}
-		else if (mousePos.x < (Screen.width/2 - 50))
+		else if (mouseXPos < (Screen.width/2 - 50))
 		{
-			this.transform.Rotate(0,-2,0);
+			this.transform.Rotate(0,-rotateSpeed,0);
 		}
 	}
 }
