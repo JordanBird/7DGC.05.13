f23e6d20744db2c823e3b343edb433d3f98e1297
Jordan Bird <jordanbird-29@hotmail.co.uk>
2013-05-25 15:33:07 +0100

    Final Outside Commit

--- Jordan's Stuff/Assets/cscript_point.cs
+++ Jordan's Stuff/Assets/cscript_point.cs
@@ -6,6 +6,9 @@ public class cscript_point : MonoBehaviour {
 	public int electricity = 2;
 	public int steam = 3;
 	
+	public int captureRequirement = 100;
+	public int captureProgress = 0;
+	
 	public cscript_player ownedPlayer;
 	// Use this for initialization
 	void Start () {
@@ -14,7 +17,81 @@ public class cscript_point : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		ownedPlayer.AddElectricity(electricity);
-		ownedPlayer.AddSteam(steam);
+		if (captureProgress == captureRequirement)
+		{
+			ownedPlayer.AddElectricity(electricity);
+			ownedPlayer.AddSteam(steam);
+		}
+		
+		if (CheckForUnits() == true)
+		{
+			foreach (GameObject g in GetCollidingUnits())
+			{
+				if (captureProgress == 0)
+				{
+					ownedPlayer = (new cscript_unit = g).GetOwnedPlayer();
+				}
+				
+				if ((new cscript_unit = g).GetOwnedPlayer() == ownedPlayer)
+				{
+					RaiseCaptureProgress(1);
+				}
+				else
+				{
+					LowerCaptureProgress(1);
+				}
+			}
+		}
+		
+		if (captureProgress == 0)
+			ownedPlayer = null;
+	}
+	
+	public void LowerCaptureProgress(int i)
+	{
+		captureProgress -= i;
+		
+		if (captureProgress < 0)
+			captureProgress = 0;
+	}
+	
+	public void RaiseCaptureProgress(int i)
+	{
+		captureProgress += i;
+		
+		if (captureProgress > captureRequirememnt)
+			captureProgress = captureRequirememnt;
+	}
+	
+	public void ChangeOwnedPlayer(cscript_player p)
+	{
+		ownedPlayer = p;
+	}
+	
+	public bool CheckForUnits()
+	{
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, 10);
+		
+		foreach (Collider c in hitColliders)
+		{
+			if (c.gameObject.tag == "Unit")
+				return true;
+		}
+		
+		return false;
+	}
+	
+	public List<GameObjects> GetCollidingUnits()
+	{
+		List<GameObjects> output = new List<GameObjects>();
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, 10);
+		
+		foreach (Collider c in hitColliders)
+		{
+			if (c.gameObject.tag == "Unit")
+				output.Add(c.gameObject);
+		}
+		
+		return output;
 	}
 }
